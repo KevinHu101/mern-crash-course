a6f5ca7434f5b2c528a7a8fdea3f568cfe4f9f99
AEILNOUYZ <hellojava3301@gmail.com>
2025-01-22 23:06:51 -0800

    project completed

---
+++ backend/routes/product.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  deleteProduct,
+  getProduct,
+  postProduct,
+  updateProduct,
+} from "../controllers/product.controller.js";
+
+const router = express.Router();
+
+router.get("/", getProduct);
+router.post("/", postProduct);
+router.delete("/:id", deleteProduct);
+router.put("/:id", updateProduct);
+
+export default router;
